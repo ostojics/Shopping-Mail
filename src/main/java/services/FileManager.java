29de28b9c93ba3417a811a27d484aaea9c88bdb2
ostojics <ostojic.dev@gmail.com>
2021-12-11 22:03:27 +0100

    Added login functionality

--- src/main/java/services/FileManager.java
+++ src/main/java/services/FileManager.java
@@ -8,6 +8,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.Arrays;
 import models.User;
 
 /**
@@ -29,18 +30,42 @@ public class FileManager {
         }   
     }
 
-    public void readUsers() {
+    public boolean isValidLogin(String username, String password) {
+        boolean isValid = false;
+        
         try {
+            //standard file readed in java
             BufferedReader reader = new BufferedReader(new FileReader("data" + File.separator + "users.txt"));
             String line;
-             
+            
+            //loop trough lines in the file until we reach the end of file(EOF)
              while ((line = reader.readLine()) != null) {
-                 System.out.println(line);
+                //create username and password string that the user entered
+                //so we can compare them to the users in the database
+                String usernameMatcher = String.format("Username:%s", username);
+                String passwordMatcher = String.format("Password:%s", password);
+                String[] split = line.split("\\s+");
+                String currentUsername = "";
+                String currentPassword = "";
+                
+                if(split.length > 1) {
+                   //current username and password that we are checking in the database
+                   currentUsername = split[4];
+                   currentPassword = split[5];
+                }
+                
+                if(currentUsername.equals(usernameMatcher) && currentPassword.equals(passwordMatcher)) {
+                    isValid = true;
+                    reader.close();
+                    return isValid;
+                }
             }
              
             reader.close();
         } catch (Throwable throwable) {
             throwable.printStackTrace();
         }
+        
+        return isValid;
     }
 }
